0e889b08e1153b93ef5c9772f6c517ce190e8df9
Mafaz <mafazfathma3@gmail.com>
2025-09-30 11:41:08 +0530

    add color

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -25,7 +25,8 @@ onMounted(() => {
       showGridlines
       emptyMessage="No tickets available"
     >
-      <Column field="transactionId" header="Transaction ID"></Column>
+      <Column selectionMode="multiple" headerStyle="width: 3px height=13px"></Column>
+     <Column field="transactionId" header="Transaction ID"></Column>
       <Column field="ticketId" header="Ticket ID"></Column>
       <Column field="createdOn" header="Created On"></Column>
       <Column field="completedBy" header="Completed By"></Column>
@@ -43,3 +44,45 @@ onMounted(() => {
     </DataTable>
   </div>
 </template>
+<style scoped>
+:deep(.p-datatable-thead > tr > th) {
+  background-color: #E7E7E7 !important;
+  color: #01135D !important;
+  font-family:'Kanit' !important;
+  text-transform: capitalize;
+  font-size: 10px;
+  vertical-align: middle !important;
+  text-align: center; 
+  padding-top: 8px;
+  padding-bottom: 8px;
+}
+
+:deep(.p-datatable-tbody > tr > td) {
+  font-family: 'Kanit' !important;
+  vertical-align: middle !important;
+  font-size: 14px;
+  text-align: center; 
+  color: #25213B;
+  border-left: none !important;
+  border-right: none !important;
+  border-bottom: 1px solid #ccc !important;
+}
+:deep(.p-checkbox-box) {
+    width: 13px !important;
+    height: 13px !important;
+    border-width: 0.98px !important; 
+    border-radius: 1.95px !important; 
+}
+:deep(.p-paginator) {
+   
+    width: 1375px !important; 
+    height: 45px !important; 
+    display: flex !important;
+    justify-content: flex-start !important; 
+    align-items: center !important; 
+    text-align: left !important; 
+}
+
+
+</style>
+
